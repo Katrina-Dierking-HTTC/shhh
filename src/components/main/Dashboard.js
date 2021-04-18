d77a288dd22a87ec330a5ecdb03f06b0bdbffdf9
Katrina Dierking <kalynndier@gmail.com>
2021-04-18 00:37:18 -0400

    making changes to home page and guide

--- src/components/main/Dashboard.js
+++ src/components/main/Dashboard.js
@@ -24,16 +24,16 @@ function  Dashboard({children}) {
                 <List>
                     <List.Item>
                         <List.Icon name='users' />
-                        <List.Content>Semantic UI</List.Content>
+                        <List.Content>Her Time to Code</List.Content>
                     </List.Item>
                     <List.Item>
                         <List.Icon name='marker' />
-                        <List.Content>New York, NY</List.Content>
+                        <List.Content>North Carolina</List.Content>
                     </List.Item>
                     <List.Item>
                         <List.Icon name='mail' />
                         <List.Content>
-                            <a href='mailto:jack@semantic-ui.com'>jack@semantic-ui.com</a>
+                            <a href='mailto:katrina@hertimetocode.com'>jack@semantic-ui.com</a>
                         </List.Content>
                     </List.Item>
                     <List.Item>
--- src/components/signup/FormSignup.js
+++ src/components/signup/FormSignup.js
@@ -78,7 +78,7 @@ const FormSignup = ({submitForm}) => {
             </div>
             <button className = 'form-input-btn'
                 type='submit'>Register</button>
-            <span className = 'form-input-login'>Already have an account? Login<Link to= "/login">here</Link></span>
+            <span className = 'form-input-login'>Already have an account? Login <Link to= "/login">here</Link></span>
 
             </form>
         </div>
